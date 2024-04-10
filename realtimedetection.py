91369a6411e7ba4f5548ef8a1ded211c94cfb051
SaifySheikh <sheikhmm_4@rknec.edu>
2024-04-10 09:29:51 +0530

    model trined on big epoch added

--- realtimedetection.py
+++ realtimedetection.py
@@ -17,7 +17,7 @@ model = model_from_json(model_json)
 model.load_weights("signlanguagedetectionmodel48x48.h5")
 
 # Define the label names
-label=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','blank']
+label=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I','J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','blank']
 
 def extract_features(image):
     feature = np.array(image)
